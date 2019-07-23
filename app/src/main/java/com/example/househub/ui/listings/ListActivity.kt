2e406f10671b9dd0f856749335a7ff092955346f
zachbirri <34723297+zachbirri@users.noreply.github.com>
2019-07-23 14:19:44 -0400

    start on listings activity

---
+++ app/src/main/java/com/example/househub/ui/listings/ListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.househub.ui.listings
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.househub.R
+
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
