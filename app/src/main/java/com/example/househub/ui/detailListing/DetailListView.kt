a7633cc754ec218497153bd78f67890729225188
JT Salisbury <crazyscouter1@gmail.com>
2019-07-25 15:19:34 -0400

    Begin detail view

--- app/src/main/java/com/example/househub/ui/detailListing/DetailListView.kt
+++ app/src/main/java/com/example/househub/ui/detailListing/DetailListView.kt
@@ -3,11 +3,15 @@ package com.example.househub.ui.detailListing
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import com.example.househub.HTTPRequest
+import com.example.househub.JWT
 import com.example.househub.R
 
 import kotlinx.android.synthetic.main.activity_detail_list_view.*
 
-class DetailListView : AppCompatActivity() {
+abstract class DetailListView : AppCompatActivity() {
+
+    abstract var listing: Map<String, String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +23,25 @@ class DetailListView : AppCompatActivity() {
                 .setAction("Action", null).show()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val pid = getIntent().getIntExtra("pid", 0)
+        val uid = getIntent().getIntExtra("uid", 0)
+
+        val jwt = JWT()
+        val payload = mapOf("pid" to pid, "requesterid" to uid)
+
+        val req = HTTPRequest("http://u747950311.hostingerapp.com/househub/api/listings/retrieve.php", payload, "", "")
+        val results = req.open()
+
+        val success = results.first
+        val fail    = results.second
+
+        if (fail != "" || !jwt.verifyToken(success))
+            return
+
+        val data = jwt.decodePayload(success)
+
+
     }
 
 }
