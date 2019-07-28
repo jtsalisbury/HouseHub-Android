f9d3c0c8462860edf609fec071b9fa500012d3cd
pragarct <pragarct@mail.uc.edu>
2019-07-28 14:28:50 -0400

    ListView Complete - Added Custom List Items

--- app/src/main/java/com/example/househub/ui/listings/ListViewToolbarActivity.kt
+++ app/src/main/java/com/example/househub/ui/listings/ListViewToolbarActivity.kt
@@ -1,20 +1,25 @@
 package com.example.househub.ui.listings
 
+import android.content.Context
 import android.support.design.widget.NavigationView
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.*
 import com.example.househub.HTTPRequest
 import com.example.househub.JWT
 import com.example.househub.R
 import com.example.househub.data.model.Listing
+import com.example.househub.ui.listings.*
 import com.example.househub.ui.account.AccountInfoActivity
 import com.example.househub.ui.detailListing.DetailListView
 import com.example.househub.ui.login.LoginActivity
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_listview_toolbar.*
 import kotlinx.android.synthetic.main.toolbar.drawer_layout
 import kotlinx.android.synthetic.main.toolbar.navigation_view
@@ -227,8 +232,64 @@ open class ListViewToolbarActivity : AppCompatActivity() {
                 val listing = listings[i]
                 listItems[i] = listing.title
             }
-            val adapter = ArrayAdapter(baseContext, android.R.layout.simple_list_item_1, listItems)
+            val adapter = ListingAdapter(baseContext, listings)
             listView.adapter = adapter
+        }
+    }
+
+    class ListingAdapter(context: Context, dataSource: List<Listing>) : BaseAdapter() {
+
+        private val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        private val listings = dataSource
+
+        //1
+        override fun getCount(): Int {
+            return listings.size
+        }
+
+        //2
+        override fun getItem(position: Int): Any {
+            return listings[position]
+        }
+
+        //3
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        //4
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            // Get view for row item
+            val rowView = inflater.inflate(R.layout.listview_listing_item, parent, false)
+
+            // Get thumbnail element
+            val thumbnailImageView = rowView.findViewById(R.id.listingImage) as ImageView
+
+            // Get title element
+            val titleTextView = rowView.findViewById(R.id.listingTitle) as TextView
+
+            // Get subtitle element
+            val locationTextView = rowView.findViewById(R.id.listingLocation) as TextView
+
+            // Get subtitle element
+            val priceTextView = rowView.findViewById(R.id.listingPrice) as TextView
+
+            // Get detail element
+            val dateCreatedTextView = rowView.findViewById(R.id.listingDateCreated) as TextView
+
+            // 1
+            val listing = getItem(position) as Listing
+
+            val url = "http://u747950311.hostingerapp.com/househub/api/images"
+            Picasso.get().load(url + "/" + listing.pid + "/0.jpg").into(thumbnailImageView)
+
+            titleTextView.text = listing.title
+            locationTextView.text = listing.loc
+            val price = "$" + listing.base_price
+            priceTextView.text = price
+            dateCreatedTextView.text = listing.created
+
+            return rowView
         }
     }
 }
